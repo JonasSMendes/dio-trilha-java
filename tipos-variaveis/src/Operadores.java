9b134c3cc21110f49229e45ba0597059e63c8486
jonas mendes <jonas.jsm2903@gmail.com>
2024-05-01 10:10:25 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,20 @@
+
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 5;
+        b= 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+
+//        == igual
+//        => maior ou igual
+//        =< menor ou igual
+//        && se
+//        || ou
+
+
+        System.out.println(resultado);
+    }
+}
